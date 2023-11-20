f8e9883eaf483bded203d86b0d0b7b6fa680c22f
mdpawar <manojdpawar@outlook.com>
2023-11-21 02:38:26 +0530

    creating response to browser

--- NodeJS/server/app.js
+++ NodeJS/server/app.js
@@ -2,7 +2,15 @@ const http = require('http');
 
 
 const server = http.createServer((req, res) => {
-    console.log('Manoj Dhananjay Pawar')
+    console.log(req.url, req.method, req.headers)
+    // console.log('Manoj Dhananjay Pawar')
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Welcome to My Node JS Project</title><head>');
+    res.write('<body><h1> Welcome to My Node Js Project</h1></body>')
+    res.write('<html>');
+    res.end();
 });
 
 server.listen(4000);
