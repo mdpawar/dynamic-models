0289bfc4875fd8512272d128831709f40f3769e0
mdpawar <manojdpawar@outlook.com>
2023-11-21 13:16:35 +0530

    Routing request

--- NodeJS/server/app.js
+++ NodeJS/server/app.js
@@ -1,10 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers)
+    // console.log(req.url, req.method, req.headers)
     // console.log('Manoj Dhananjay Pawar')
     // process.exit();
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+         res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="POST" ><input type="text name="message"><button type="submit>Send</button></input></form></body>')
+        res.write('<html>');
+        return res.end(); // this will end function exection here only otherwise it wil continue further.
+    }
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMY');   
+        res.statusCode = 302;
+        res.setHeader('location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Welcome to My Node JS Project</title><head>');
