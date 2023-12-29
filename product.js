c9640eac7cba3981ad6faf67eb10a34d132c4361
mdpawar <139568246+mdpawar@users.noreply.github.com>
2023-12-29 12:05:18 +0530

    SQL Functionality for CRUD

--- product.js
+++ product.js
@@ -1,23 +1,24 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs'); not require when using sql
+// const path = require('path');
 
+const db = require('../util/databse');
 const Cart = require('./cart');
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
+// const p = path.join(
+//   path.dirname(process.main.filename),
+//   'data',
+//   'products.json'
+// ); NOT REQUIRED WHEN SUING SQL
 
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
+// const getProductsFromFile = cb => {
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     } else {
+//       cb(JSON.parse(fileContent));
+//     }
+//   });
+// }; NOT REQUIRED WHEN USING SQL
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -29,46 +30,58 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products => {
-      if (this.id) {
-        const existingProductIndex = products.findIndex(
-          prod => prod.id === this.id
-        );
-        const updatedProducts = [...products];
-        updatedProducts[existingProductIndex] = this;
-        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-          console.log(err);
-        });
-      } else {
-        this.id = Math.random().toString();
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err);
-        });
-      }
-    });
+    return db.execute(
+      'INSERT INTO products (title, price, imageURL, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    )
+    // getProductsFromFile(products => {
+    //   if (this.id) {
+    //     const existingProductIndex = products.findIndex(
+    //       prod => prod.id === this.id
+    //     );
+    //     const updatedProducts = [...products];
+    //     updatedProducts[existingProductIndex] = this;
+    //     fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+    //       console.log(err);
+    //     });
+    //   } else {
+    //     this.id = Math.random().toString();
+    //     products.push(this);
+    //     fs.writeFile(p, JSON.stringify(products), err => {
+    //       console.log(err);
+    //     });
+    //   }
+    // });
+
   }
 
   static deleteById(id) {
-    getProductsFromFile(products => {
-      const product = products.find(prod => prod.id === id);
-      const updatedProducts = products.filter(prod => prod.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if (!err) {
-          Cart.deleteProduct(id, product.price);
-        }
-      });
-    });
+
+    return db.execute('DELETE FROM products WHERE id = ?', [id]);
+    // getProductsFromFile(products => {
+    //   const product = products.find(prod => prod.id === id);
+    //   const updatedProducts = products.filter(prod => prod.id !== id);
+    //   fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+    //     if (!err) {
+    //       Cart.deleteProduct(id, product.price);
+    //     }
+    //   });
+    // });
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static fetchAll() {
+
+    // getProductsFromFile(cb);
+
+    return db.execute('SELECT * FROM products');
   }
 
-  static findById(id, cb) {
-    getProductsFromFile(products => {
-      const product = products.find(p => p.id === id);
-      cb(product);
-    });
+  static findById(id) {
+
+    return db.execute('SELECT * FROM products WHERE products.id = ?'[id]);
+    // getProductsFromFile(products => {
+    //   const product = products.find(p => p.id === id);
+    //   cb(product);
+    // });
   }
 };
--- shop.js
+++ shop.js
@@ -1,23 +1,69 @@
-const path = require('path');
+const Product = require('../models/product');
+const Cart = require('../models/cart');
 
-const express = require('express');
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+  .then((rows, fieldData) => {
+    res.render('shop/index', {
+      prods: rows,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  })
+  .catch(err => console.log(err))
+};
 
-const shopController = require('../controllers/shop');
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then(([product]) => {
+      res.render('shop/product-detail', {
+        product: product[0],
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err))
+};
 
-const router = express.Router();
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll()
+  .then((rows, fieldData) => {
+    res.render('shop/index', {
+      prods: rows,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  })
+  .catch(err => console.log(err))
+    
+  };
 
-router.get('/', shopController.getIndex);
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
 
-router.get('/products', shopController.getProducts);
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
 
-router.get('/products/:productId', shopController.getProduct);
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
 
-router.get('/cart', shopController.getCart);
-
-router.post('/cart', shopController.postCart);
-
-router.get('/orders', shopController.getOrders);
-
-router.get('/checkout', shopController.getCheckout);
-
-module.exports = router;
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
